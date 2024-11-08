83e481cc15da9c797da584ab787881499de8a9cd
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-11-08 13:34:37 +0530

    Updated Database URL

--- Backend/Database_and_ORM/Methods.py
+++ Backend/Database_and_ORM/Methods.py
@@ -7,7 +7,7 @@ from decouple import config
 async def init_db():
     try:
         await Tortoise.init(
-            db_url=f"mysql://{config("DATABASE_USERNAME")}:{config("DATABASE_PASSWORD")}@{config("DATABASE_HOST")}:{config("DATABASE_PORT")}/{config("DATABASE_NAME")}",
+            db_url=f"mysql://{config("DATABASE_USERNAME")}:{config("DATABASE_PASSWORD")}@{config("DATABASE_HOST")}:{config("DATABASE_PORT")}/{config("DATABASE_NAME")}?minsize={config("MINIMUM_NUMBER_OF_CONCURRENT_DATABASE_CONNECTIONS_IN_THE_POOL")}&maxsize={config("MAXIMUM_NUMBER_OF_CONCURRENT_DATABASE_CONNECTIONS_IN_THE_POOL")}",
             modules={"models": ["User"]},
         )
         await Tortoise.generate_schemas(safe=True)
