154d4aec72a3c5eb6137b643a7e3c3abc5d1e7bd
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-11-11 16:26:25 +0530

    Updated order of declaration
    Updated order of declaration of lifespan event handlers w.r.t to the other elements

--- Backend/Amenity_Tracking_API.py
+++ Backend/Amenity_Tracking_API.py
@@ -1,25 +1,10 @@
 # main.py
 from fastapi import FastAPI
-from Database_and_ORM.Methods import init_db, close_db
+from Database_and_ORM.Database_Connector import init_db, close_db
 from Users.Router import User_Router  # Import the user router
 from decouple import config
 from Methods import add_cors_middleware, add_api_key_middleware
 from contextlib import asynccontextmanager
-
-app = FastAPI(title="Amenity Tracking API")
-
-add_cors_middleware(app)
-add_api_key_middleware(app)
-
-
-# Global route
-@app.get("/")
-async def root():
-    return {"message": "Welcome to the Amenity Tracking API"}
-
-
-# Registering the user router
-app.include_router(User_Router, prefix="/users", tags=["Users"])
 
 
 @asynccontextmanager
@@ -31,4 +16,19 @@ async def lifespan(app: FastAPI):
     await close_db()
 
 
-app = FastAPI(lifespan=lifespan)
+# Initialize the app with lifespan
+app = FastAPI(title="Amenity Tracking API", lifespan=lifespan)
+
+# Add middlewares
+add_cors_middleware(app)
+add_api_key_middleware(app)
+
+
+# Global route
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Amenity Tracking API"}
+
+
+# Register the user router
+app.include_router(User_Router, prefix="/users", tags=["Users"])
