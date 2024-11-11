6d66fc550491b7b13a33dc6afc5e5d88cc53fc60
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-11-11 15:45:31 +0530

    Updated formatting

--- Backend/Users/Methods.py
+++ Backend/Users/Methods.py
@@ -37,8 +37,7 @@ def create_jwt_token(user_id: str):
     """
     payload = {
         "user_id": user_id,
-        "exp": datetime.utcnow()
-        + timedelta(hours=24),  # Token valid for 1 day
+        "exp": datetime.utcnow() + timedelta(hours=24),  # Token valid for 1 day
     }
     token = jwt.encode(payload, config("JWT_SECRET_STRING"), algorithm="HS256")
     return token
