3e92377e154324bf3597e9b3c3504564a19806fb
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-11-12 13:57:06 +0530

    Implemented 2FA, Password Reset, Comm Verification

--- Backend/Utility_Methods/Utility_Methods.py
+++ Backend/Utility_Methods/Utility_Methods.py
@@ -1,7 +1,10 @@
 from fastapi import HTTPException, status, Header
-from Database_and_ORM.Database_Models import Blacklisted_Tokens
+from Users.Data_Schemas import OTPTypeEnum
+from Database_and_ORM.Database_Models import Blacklisted_Tokens, OTP
 from decouple import config
 import jwt
+import random
+from datetime import datetime, timedelta
 
 
 async def get_token_from_authorization_header_value(
@@ -45,3 +48,43 @@ async def verify_jwt(authorization: str = Header(None)):
         )
 
     return payload  # Return the decoded payload if the token is valid
+
+
+async def generate_random_otp(length: int = 6) -> int:
+    """Generates a random numeric OTP of specified length."""
+    return "".join([str(random.randint(0, 9)) for _ in range(length)])
+
+
+async def create_jwt(user_id: str, expiration_duration: int) -> str:
+    """
+    Generates a JWT token containing the user ID and expiration date.
+    """
+    payload = {
+        "user_id": user_id,
+        "exp": datetime.utcnow()
+        + timedelta(minutes=expiration_duration),  # Token valid for 1 day
+    }
+    token = jwt.encode(payload, config("JWT_SECRET_STRING"), algorithm="HS256")
+    return token
+
+
+async def verify_otp(
+    user_id: str, otp_code: int, purpose: OTPTypeEnum
+) -> bool:
+    """
+    Verifies an OTP for a specific user and purpose. If valid, deletes the OTP.
+    """
+    otp_entry = await OTP.get_or_none(
+        user_id=user_id, otp_code=otp_code, purpose=purpose
+    )
+
+    # Check OTP existence and expiration
+    if otp_entry and otp_entry.expiration > datetime.utcnow():
+        # OTP is valid; delete it after successful verification
+        await otp_entry.delete()
+        return True
+
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Invalid or expired OTP",
+    )
