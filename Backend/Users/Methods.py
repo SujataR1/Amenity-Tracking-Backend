b75f7a443f5f621b5239da43104e73fc04a748aa
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-11-14 14:04:12 +0530

    Updated image validation and limit

--- Backend/Users/Methods.py
+++ Backend/Users/Methods.py
@@ -425,6 +425,22 @@ async def upload_profile_picture(payload: dict, file: UploadFile) -> dict:
     )
     os.makedirs(directory, exist_ok=True)
 
+    if file.content_type not in ["image/jpeg", "image/png", "image/jpg"]:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Only JPEG and PNG images are allowed.",
+        )
+
+    # Check file size (limit 500kB)
+    file_size = await file.read()  # read content to check size
+    if len(file_size) > config("MAXIMUM_IMAGE_SIZE") * 1024 * 1024:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"File size should not exceed {config("MAXIMUM_IMAGE_SIZE")} mBs.",
+        )
+
+    await file.seek(0)
+
     # Create the file path
     file_path = os.path.join(directory, f"{user_id}_{file.filename}")
 
