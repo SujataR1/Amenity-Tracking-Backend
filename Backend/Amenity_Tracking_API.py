4dbf1ea01071d2cb1f8b5e30070970d6736ea3da
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-11-14 12:33:31 +0530

    Updates

--- Backend/Amenity_Tracking_API.py
+++ Backend/Amenity_Tracking_API.py
@@ -33,7 +33,9 @@ middlewares = [
     # Middleware(APITrackingMiddleware),
 ]
 
-app = FastAPI(title="Amenity Tracking API", lifespan=lifespan, middleware=middlewares)
+app = FastAPI(
+    title="Amenity Tracking API", lifespan=lifespan, middleware=middlewares
+)
 
 
 # Global route
