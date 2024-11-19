428ef3736bd77c1d9a9bbe6cfc6802dacc6cee84
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-11-19 11:54:05 +0530

    Updated router inclusion

--- Resource_Consumption/Router.py
+++ Resource_Consumption/Router.py
@@ -9,18 +9,30 @@ from .Fuel_Consumption_Router import Fuel_Consumption_Router
 # Create a parent router for all consumption-related routers
 Consumption_Router = APIRouter()
 
-# Include the individual routers into the parent router
-Consumption_Router.include_router(
-    Electricity_Consumption_Router,
-    prefix="/electricity",
-    tags=["Electricity Consumption"],
-)
-Consumption_Router.include_router(
-    Gas_Consumption_Router, prefix="/gas", tags=["Gas Consumption"]
-)
-Consumption_Router.include_router(
-    Water_Consumption_Router, prefix="/water", tags=["Water Consumption"]
-)
-Consumption_Router.include_router(
-    Fuel_Consumption_Router, prefix="/fuel", tags=["Fuel Consumption"]
-)
+routers = [
+    {
+        "router": Electricity_Consumption_Router,
+        "prefix": "/electricity",
+        "tags": ["Electricity Consumption"],
+    },
+    {
+        "router": Gas_Consumption_Router,
+        "prefix": "/gas",
+        "tags": ["Gas Consumption"],
+    },
+    {
+        "router": Water_Consumption_Router,
+        "prefix": "/water",
+        "tags": ["Water Consumption"],
+    },
+    {
+        "router": Fuel_Consumption_Router,
+        "prefix": "/fuel",
+        "tags": ["Fuel Consumption"],
+    },
+]
+
+for route in routers:
+    Consumption_Router.include_router(
+        route["router"], prefix=route["prefix"], tags=route["tags"]
+    )
