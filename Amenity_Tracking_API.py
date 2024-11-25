6ac70fd7f86833c261af2a77027d55ebc61c40c7
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-11-25 13:23:28 +0530

    Updated stuff

--- Amenity_Tracking_API.py
+++ Amenity_Tracking_API.py
@@ -5,6 +5,7 @@ from Users.Router import User_Router
 from Admin.Router import Admin_Router
 from Questionnaire.Router import Questionnaire_Router
 from Resource_Consumption.Router import Consumption_Router
+from Projections.Router import Projection_Router
 from decouple import config
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware import Middleware
@@ -51,6 +52,7 @@ routers = [
     (Questionnaire_Router, "/questionnaire", ["Questionnaire"]),
     (Admin_Router, "/admin", ["Admin"]),
     (Consumption_Router, "/consumption", ["Consumption"]),
+    (Projection_Router, "/projection", ["Projection"]),
 ]
 
 for router, prefix, tags in routers:
