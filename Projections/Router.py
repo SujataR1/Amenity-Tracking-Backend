b19b8f85b3887945abbe00b1fc232e988100558f
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-11-25 13:25:36 +0530

    Updated stuff

--- Projections/Router.py
+++ Projections/Router.py
@@ -2,11 +2,9 @@ from fastapi import APIRouter, HTTPException, Depends
 from .Data_Schemas import ProjectionRequest
 from .Methods import (
     predict_consumption,
-    get_training_status_from_file,
 )  # Import the method
 from Utility_Methods.Utility_Methods import verify_jwt
 from os import path
-import json
 from decouple import config
 
 Projection_Router = APIRouter()
